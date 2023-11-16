70b11cc8f5b3fd24dc917a805ac0404941b016a2
tayyabhameed <144664557+tayyabhameed@users.noreply.github.com>
2023-11-16 00:22:25 -0800

    Update class.js

--- class.js
+++ class.js
@@ -1,1 +1,12 @@
 class in js
+  <script>
+  class info{
+    constructor constructor (fName,lName,id){
+   this.fName=fName;
+   this.lName=lName;
+   this.id=id;
+    }
+  }
+ const info1=newsnfo("Ahad","Asaf",2)
+   console.log(info1);
+  </script>
